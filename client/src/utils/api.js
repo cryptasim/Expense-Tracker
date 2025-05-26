afa1fcbb816143c72bd60a93146e3fd905c729a8
cryptasim <128727510+cryptasim@users.noreply.github.com>
2025-05-27 02:17:00 +0530

    Update api.js -for deployment

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/api",  
+  baseURL: "https://expense-tracker-server-l0az.onrender.com/api",  
   headers: { "Content-Type": "application/json" },
   withCredentials: true,
 });
