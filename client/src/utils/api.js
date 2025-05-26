812a04f67a582c792f5a08089188da327b4987c1
cryptasim <128727510+cryptasim@users.noreply.github.com>
2025-05-27 02:26:56 +0530

    Update api.js -for deployment

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://expense-tracker-server-l0az.onrender.com/api",  
+  baseURL: "process.env.REACT_APP_API_URL",  
   headers: { "Content-Type": "application/json" },
   withCredentials: true,
 });
